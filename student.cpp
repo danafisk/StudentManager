b9a14b41e2ae92d183de30ca8d9f9e2cc03beb64
danafisk <dfisk@csuchico.edu>
2018-12-03 16:51:48 -0800

    Added Studnet Class

---
+++ student.cpp
@@ -0,0 +1,14 @@
+#include "student.h"
+
+void student:: setName(std::string firstname, std::string lastname) 
+{
+  first = firstname;
+  last = lastname;
+fullName()
+
+{
+  std::string str = "";
+  str.append(first).append(" ").append(last)
+  return str;
+}
+}
---
+++ student.h
@@ -0,0 +1,19 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+
+  class student
+{
+  private:
+
+  std::string first;
+  std::string last;
+
+  public:
+
+void setNAme(std::string, std::string)
+};
+
+
+
+#endif
